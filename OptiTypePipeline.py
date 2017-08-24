6b9e469f2368ef775dde05470f554fba2a057cdd
b-schubert <benni.schubert@gmail.com>
2017-08-24 10:41:11 -0400

    Update OptiTypePipeline.py

--- OptiTypePipeline.py
+++ OptiTypePipeline.py
@@ -7,7 +7,7 @@ OptiType: precision HLA typing from next-generation sequencing data
 ###################################################################
 
 Authors: András Szolek, Benjamin Schubert, Christopher Mohr
-Date: April 2014
+Date: August 2017
 Version: 1.3.1
 License: OptiType is released under a three-clause BSD license
 
